bb22b3375bf5b0c4abc1fb74f7142b136ae062d0
Lamiya05 <143430252+Lamiya05@users.noreply.github.com>
2024-10-15 18:41:28 +0400

    Add files via upload

---
+++ My17App.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class My17App {
+    public static void main(String[] args) {
+        int a, b, sum = 0, count = 0;
+        double ortalama;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("b = ");
+        b = input.nextInt();
+
+        for (a=1; a <= b; a++) {
+            if (a % 3 == 0 && a % 4 == 0){
+                System.out.println(a);
+                sum+=a;
+                count++;
+            }
+        }
+
+        if (count > 0){
+            ortalama = sum / count;
+            System.out.print("ortalama " + ortalama);
+        }else{
+            System.out.print("eded yoxdur");
+        }
+    }
+}
